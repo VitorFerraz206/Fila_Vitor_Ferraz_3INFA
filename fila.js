b425645cd4256b04816972083c666d3ee1fcde05
VitorFerraz206 <124786273+VitorFerraz206@users.noreply.github.com>
2024-03-25 23:05:45 -0300

    Add files via upload

---
+++ fila.js
@@ -0,0 +1,41 @@
+// 1º a entrar é 1º a sair
+
+const alunos = [
+    "Henrique",
+    "Haroldo",
+    "Pedro",
+    "Vitor",
+]
+
+
+// Criar fila --> criarFila(tamanho = int):any[] --> retorna qualquer coisa
+export function criarFila(tamanho = 5){
+    console.log("Fila criada")
+    return [... new Array(tamanho)]
+}   
+criarFila()
+
+
+// Inserir na fila --> inserirFila(fila:any[], item):void --> passa dois parâmetros:
+// fila que é uma array de qualquer coisa; e retorna vazio
+export function inserirNaFila(fila, item){ 
+    const posicao = fila.indexOf(undefined)
+        if (posicao === -1) { // Se não tiver espaço na fila
+            console.log("Não possui espaço")
+            return
+        }
+    fila[posicao] = item 
+    return
+}
+inserirNaFila()
+
+// Retirar da fila --> remover(fila:any[]):any[] --> passa um parâmetro:
+// fila que é uma array de qualquer coisa; e retorna qualquer coisa
+export function removerDaFila(fila){ 
+    if(fila[0] === undefined){
+        console.log("Fila Vazia")
+        return
+    }
+    fila.pop([0])
+}
+removerDaFila()
